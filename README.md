11d1c3a014a03f2b716cab807059503288fbbcbd
Mikhail Kyraha <1049133+kyraha@users.noreply.github.com>
2021-02-08 19:45:30 -0600

    Works as G-Functions

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # TBA-push-to-Slack
 A Google AppEngine code to receive push notifications from The Blue Alliance and post them on Slack via push notifications as well.
+
+# Google Cloud Functions
+Starting 2021 this code got rewritten to run on GCS Cloud Functions rather than on GCS App Engine.
+Functions is a simpler, one function focused, microservices kind of platform.
+No need to deploy a whole App Engine to process simple push requests from TBA and perform 
+another own push request to Slack.
+
+If you are familiar with old good AWS Lambda Functions. GC Functions is pretty much the same thing.
+
+# Setup
+
+Deploy the content of main.py as a Google Cloud Function using Python3 (tested with Python 3.7).
+Set the "entryPoint" as "tba_to_slack".
+Then configure the VARIABLES as described below. Then go to your account at TBA and point your webhook
+to the Trigger URL of the Function you just deployed.
+
+# Configuration
+## Two secret variables
+* SLACK_TEST: "/services/A99A9AAA9/*DUMMY*555/d1Fake8-Fake-Fake-xyzzz"
+* SLACK_PROD: "/services/A99A9AAA9/*DUMMY*555/d1Fake8-Fake-Fake-xyzzz"
+
+## The secret that you have between you and TheBlueAlliance
+* TBA_SECRET: "ERRORS 3130 are awesome"
+
+## The time-zone in which the events time should be displayed
+* TARGET_TZ: US/Central
+
+## Your team number
+* FRC_TEAM: 3130
--- main.py
+++ main.py
@@ -1,29 +1,22 @@
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
-
 import os
 import json
 import time
+import requests
 import hashlib
-import logging
-import webapp2
-import httplib, urllib
-import yaml
 
 def post2slack(message):
+    # Secret is passed as an ENV variable. Will throw an exception if not defined.
+    url = "https://hooks.slack.com" + os.environ['SLACK_TEST']
     params = json.dumps({'text': message})
     headers = {"Content-type": "application/json", "Accept": "text/plain"}
-    conn = httplib.HTTPSConnection(config['webhook_hostname'])
-    conn.request("POST", config['webhook_request'], params, headers)
-    response = conn.getresponse()
-    ret = str(response.status) + " " + response.reason + "<br>\n"
-    ret += response.read()
-    conn.close()
+    r = requests.post(url, data=params, headers=headers)
+    ret = str(r.status_code) + " " + r.reason
+    r.close()
     return ret
 
 def unfrc(team):
     team = team.replace('frc','',1)
-    if team == str(config.get('frc_team', 0)):
+    if team == str(os.environ.get('FRC_TEAM', 0)):
         team = '*' + team + '*'
     return team
 
@@ -35,12 +28,15 @@ def parse_tba(payload):
         "sf": "Semifinals",
         "f": "Finals",
     }
-    tz_offset = config.get('tz_offset', 0)
+
+    os.environ['TZ'] = os.environ.get('TARGET_TZ', 'GMT')
+    time.tzset()
+
     message = ""
-    body = json.loads(payload)
+    body = payload
     if body['message_type'] == 'upcoming_match':
-        predicted = time.strftime("%H:%M",time.gmtime(body['message_data']['predicted_time']-tz_offset))
-        scheduled = time.strftime("%H:%M",time.gmtime(body['message_data']['scheduled_time']-tz_offset))
+        predicted = time.strftime("%H:%M",time.localtime(body['message_data']['predicted_time']))
+        scheduled = time.strftime("%H:%M",time.localtime(body['message_data']['scheduled_time']))
         message += "Upcoming match at " + predicted + "\n[ "
         count = 0
         for team in body['message_data']['team_keys']:
@@ -61,7 +57,7 @@ def parse_tba(payload):
             message += "scored " + str(body['message_data']['match']['alliances'][alliance]['score'])
             message += "\n"
     elif body['message_type'] == 'schedule_updated':
-        first_match_time = time.strftime("%H:%M",time.gmtime(body['message_data']['first_match_time']-tz_offset))
+        first_match_time = time.strftime("%H:%M",time.localtime(body['message_data']['first_match_time']))
         message += "A match added " + first_match_time + ", nothing major."
     elif body['message_type'] == 'starting_comp_level':
         message += "Competition started. Level: " + COMP_LEVELS_VERBOSE_FULL[body['message_data']['comp_level']]
@@ -75,43 +71,29 @@ def parse_tba(payload):
             count += 1
     elif body['message_type'] == 'verification':
         print("Verification code: ", body['message_data'])
+        message = "Verification code: " + body['message_data']
     elif body['message_type'] == 'ping':
         message += "Test ping from TBA: " + body['message_data']['desc']
     else:
         message += "Unprogrammed (yet) notification at "
-        message += time.asctime(time.gmtime(time.time())) + " UTC\n"
+        message += time.asctime(time.localtime(time.time())) + "\n"
         message += payload
     return message
 
-class IncomingHandler(webapp2.RequestHandler):
-    def post(self):
-        payload = self.request.body
+def tba_to_slack(request):
+    request_json = request.get_json()
+    if request.args and 'payload' in request.args:
+        payload = json.loads(request.args.get('payload'))
+    elif request_json and 'message_type' in request_json:
+        payload = request_json
+    else:
+        return f'Empty request. Nothing happened'
 
-        if config.get('tba_secret'):
-            checksum = self.request.headers.get('X-Tba-Checksum')
-            if hashlib.sha1('{}{}'.format(config.get('tba_secret'), payload)).hexdigest() != checksum:
-                self.response.write("checksum error happened")
-                print("checksum error happened")
-                return
+    tba_secret = os.environ.get('TBA_SECRET')
+    if tba_secret:
+        checksum = request.headers.get('X-Tba-Checksum')
+        if hashlib.sha1('{}{}'.format(tba_secret.encode("utf-8"), payload.encode("utf-8"))).hexdigest() != checksum:
+            print("checksum error happened")
+            return ("checksum error happened", 501)
 
-        if payload.startswith("payload="):
-            payload = payload.replace("payload=","",1)
-            payload = urllib.unquote_plus(payload)
-        self.response.write(post2slack(parse_tba(payload)))
-
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        message = 'Hello, human...<form action="/hook" method="post"><textarea name="payload"></textarea><input type="submit" value="Submit"></form>'
-        self.response.write(message)
-
-with open("secrets.yaml", 'r') as stream:
-    try:
-        config = yaml.load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
-
-app = webapp2.WSGIApplication([
-    ('/hook', IncomingHandler),
-    ('/', MainHandler),
-], debug=True)
-
+    return post2slack(parse_tba(payload))
